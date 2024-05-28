ce9e3cb33715711eba295215a7940546b692bcf1
Santiago Maldonado <santy_gm96@hotmail.com>
2024-05-28 05:47:50 -0300

    Realizados del 1 al 10

--- EjerciciosResueltos.sql
+++ EjerciciosResueltos.sql
@@ -1,13 +1,61 @@
 -- 1 Listado de todos los idiomas.
+
+Select ID, Nombre
+From Idiomas
+
 --2 Listado de todos los cursos.
+
+Select ID, IDNivel, Nombre, CostoCurso, CostoCertificacion, Estreno
+From Cursos
+
 --3 Listado con nombre, costo de inscripción (costo de curso), costo de certificación y fecha de estreno de todos los cursos.
+
+Select Nombre, CostoCurso, CostoCertificacion, Estreno
+From Cursos 
+
 --4 Listado con ID, nombre, costo de inscripción y ID de nivel de todos los cursos cuyo costo de certificación sea menor a $5000.
+
+Select ID, Nombre, CostoCurso, IDNivel
+From Cursos
+Where CostoCertificacion < 5000
+
 --5 Listado con ID, nombre, costo de inscripción y ID de nivel de todos los cursos cuyo costo de certificación sea mayor a $1200.
+
+Select ID, Nombre, CostoCurso, IDNivel
+From Cursos
+Where CostoCertificacion > 1200
+
 --6 Listado con nombre, número y duración de todas las clases del curso con ID número 6.
+
+Select Nombre, Numero, Duracion
+From Clases
+Where IDCurso = 6
+
 --7 Listado con nombre, número y duración de todas las clases del curso con ID número 10.
+
+Select Nombre, Numero, Duracion
+From Clases
+Where IDCurso = 10
+
 --8 Listado con nombre y duración de todas las clases del curso con ID número 4. Ordenado por duración de mayor a menor.
+
+Select Nombre, Duracion
+From Clases
+Where IDCurso = 4
+Order By Duracion desc 
+
 --9 Listado de cursos con nombre, fecha de estreno, costo del curso, costo de certificación ordenados por fecha de estreno de manera creciente.
+
+Select Nombre, Estreno, CostoCurso, CostoCertificacion
+From Cursos
+Order By Estreno asc
+
 --10 Listado con nombre, fecha de estreno y costo del curso de todos aquellos cuyo ID de nivel sea 1, 5, 9 o 10.
+
+Select Nombre, Estreno, CostoCurso
+From Cursos
+Where ID in (1, 5, 9, 10)
+
 --11 Listado con nombre, fecha de estreno y costo de cursado de los tres cursos más caros de certificar.
 --12 Listado con nombre, duración y número de todas las clases de los cursos con ID 2, 5 y 7. Ordenados por ID de Curso ascendente y luego por número de clase ascendente.
 --13 Listado con nombre y fecha de estreno de todos los cursos cuya fecha de estreno haya sido en el primer semestre del año 2019.
